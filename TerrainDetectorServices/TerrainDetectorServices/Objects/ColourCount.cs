10aad4b2183701d55d39691434bc0a8206acb621
Adrian Abela <aadrianabela@gmail.com>
2015-04-14 22:13:30 +0200

    Fixed issue because I am awesome

---
+++ TerrainDetectorServices/TerrainDetectorServices/Objects/ColourCount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace TerrainDetectorServices.Objects
+{
+    public class ColourCount
+    {
+        public Color Colour { get; set; }
+        public int Count {get;set;}
+
+        public ColourCount()
+        {
+            Count = 0;
+        }
+    }
+}
--- TerrainDetectorServices/TerrainDetectorServices/TerrainDetector.asmx.cs
+++ TerrainDetectorServices/TerrainDetectorServices/TerrainDetector.asmx.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Services;
+using TerrainDetectorServices.Objects;
 
 namespace TerrainDetectorServices
 {
@@ -21,21 +23,18 @@ namespace TerrainDetectorServices
     {
         private readonly List<Tuple<Color, string>> terrains;
 
-        private static readonly Color FARMLAND = Color.FromArgb(212, 229, 202);
-        private static readonly Color BUILT_UP = Color.FromArgb(240, 237, 228);
-        private static readonly Color FOREST = Color.FromArgb(201, 222, 169);
-
         public TerrainDetector()
         {
             terrains = new List<Tuple<Color, string>>();
 
-            Color FARMLAND = Color.FromArgb(212, 229, 202);
-            Color BUILT_UP = Color.FromArgb(240, 237, 228);
+            Color FARMLAND = Color.FromArgb(203, 220, 193);
+            Color BUILT_UP = Color.FromArgb(233, 229, 220);
             Color FOREST = Color.FromArgb(201, 222, 169);
 
             terrains.Add(new Tuple<Color, string>(FARMLAND, "Farmland"));
             terrains.Add(new Tuple<Color, string>(BUILT_UP, "Built Up"));
             terrains.Add(new Tuple<Color, string>(FOREST, "Forest"));
+            terrains.Add(new Tuple<Color, string>(Color.FromArgb(255, 241, 238, 230), "Beach"));
 
         }
 
@@ -45,7 +44,7 @@ namespace TerrainDetectorServices
             Bitmap map = DownloadMap(lat, lon);
 
             //Go through the map and count the pixels!
-            List<Tuple<Color, int>> colours = new List<Tuple<Color, int>>();
+            List<ColourCount> colours = new List<ColourCount>();
 
             for (int x = 0; x < map.Width; x++ )
             {
@@ -53,20 +52,20 @@ namespace TerrainDetectorServices
                 {
                     Color pixel = map.GetPixel(x, y);
 
-                    var matched = colours.FirstOrDefault(c => c.Item1.Equals(pixel));
+                    var matched = colours.FirstOrDefault(c => c.Colour.Equals(pixel));
 
                     if (matched != null)
                     {
-                        matched = new Tuple<Color, int>(pixel, matched.Item2 + 1); //Apparently tuples are immutable. Who knew...
+                        matched.Count++;
                     }
                     else
                     {
-                        colours.Add(new Tuple<Color, int>(pixel, 1));
+                        colours.Add(new ColourCount() { Colour = pixel });
                     }
                 }
             }
 
-            var pixelColour = colours.OrderByDescending(c => c.Item2).Select(c => c.Item1).FirstOrDefault();
+            var pixelColour = colours.OrderByDescending(c => c.Count).Select(c => c.Colour).FirstOrDefault();
 
             var matchedString = terrains.FirstOrDefault(t => t.Item1.Equals(pixelColour));
 
@@ -90,9 +89,16 @@ namespace TerrainDetectorServices
         {
             WebClient client = new WebClient();
 
-            using (Stream stream = client.OpenRead("https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=16&size=100x100&maptype=terrain"))
+            var url = "https://maps.googleapis.com/maps/api/staticmap?center=" + lat + "," + lon + "&zoom=16&size=100x100&maptype=terrain&format=png";
+
+            //client.DownloadFile(url,"image.pg")
+
+            using (Stream stream = client.OpenRead(url))
             {
                 Bitmap bitmap = new Bitmap(stream);
+
+                bitmap.Save("C:\\Temp\\image.png", ImageFormat.Png);
+
                 return bitmap;
             }
         }
